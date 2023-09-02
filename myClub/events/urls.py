93623f172562c90e20642a414c481b3b174114df
Aditya Naresh <adityanaresh007@gmail.com>
2023-09-02 22:39:26 +0530

    Delete fuction added to Venues and Events. Basic CRUD operations completed

--- myClub/events/urls.py
+++ myClub/events/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     path('update_venue/<venue_id>', views.update_venue, name='update-venue'),
     path('add_event',views.add_event, name='add-event'),
     path('update_event/<event_id>', views.update_event, name='update-event'),
+    path('delet_event/<event_id>', views.delete_event, name='delete-event'),
+    path('delet_venue/<venue_id>', views.delete_venue, name='delete-venue')
+
 
 ]
--- myClub/events/views.py
+++ myClub/events/views.py
@@ -7,6 +7,18 @@ from . forms import VenueForm, EventForm
 from django.http import HttpResponseRedirect
 # Create your views here.
 
+def delete_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    venue.delete()
+    return redirect('list-venues')  
+
+
+
+
+def delete_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
+    return redirect('list-events')    
 
 def update_event(request,event_id):
     event = Event.objects.get(pk=event_id)
