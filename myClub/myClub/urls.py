3038058d0e0d08b4808600cdbc9da10b7cbab8bc
Aditya Naresh <adityanaresh007@gmail.com>
2023-09-03 18:40:29 +0530

    User authetication, Login and Logout functionalities added

--- myClub/myClub/urls.py
+++ myClub/myClub/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("events.urls")),
-]
+    path('members/', include("members.urls")),
+    ]
 
 
 #Configure Admin titles
