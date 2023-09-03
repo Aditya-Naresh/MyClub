5f84aeae37519b8a595f1f3b5620ced6eb305054
Aditya Naresh <adityanaresh007@gmail.com>
2023-09-03 12:05:05 +0530

    venue list is now downloadable as PDF file

--- myClub/events/views.py
+++ myClub/events/views.py
@@ -5,9 +5,50 @@ from datetime import datetime
 from . models import Event, Venue
 from . forms import VenueForm, EventForm
 from django.http import HttpResponseRedirect, HttpResponse
+from django.http import FileResponse
+import io
 import csv
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 # Create your views here.
 
+
+# Generate PDF file
+def venue_pdf(request):
+    # Create a byte stream buffer
+    buf = io.BytesIO()
+    # Create a canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    #  Create a text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    # Designate Models
+    venues = Venue.objects.all()
+
+    lines = []
+
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.website)
+        lines.append(venue.email_address)
+        lines.append("--------------------------------")
+
+    for line in lines:
+        textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+
+    return FileResponse(buf, as_attachment=True, filename='Venue.pdf')
 
 #Generate List as CSV file
 def venue_csv(request):
